5e36c155b8d7b6ff2900e53e17c4fa494736a4e3
Suma Srivalli <morapakalasuma@gmail.com>
2025-05-28 02:34:24 +0530

    Added ProductPage

---
+++ src/main/java/com/qa/opencarts/pages/ProductPage_Practice.java
@@ -0,0 +1,11 @@
+package com.qa.opencarts.pages;
+
+public class ProductPage_Practice {
+	int i=20;
+	String hash = "Harish";
+	boolean han = true;
+	public static void main(String[] args) {
+		System.out.println(i);
+	}
+
+}
