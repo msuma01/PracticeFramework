0a7906e66efb3b6bb1d262443bc4d3b687533b90
Suma Srivalli <morapakalasuma@gmail.com>
2025-05-27 23:34:50 +0530

    Modified DemoPage

--- src/main/java/com/qa/opencarts/pages/DemoPage.java
+++ src/main/java/com/qa/opencarts/pages/DemoPage.java
@@ -3,8 +3,10 @@ package com.qa.opencarts.pages;
 public class DemoPage {
 
 	public static void main(String[] args) {
-		int i =10;
+		String name="Manasa";
+		int i=10;
 		System.out.println(i+4);
+		System.out.println(name);
 
 	}
 
