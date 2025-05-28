76c74ba0d42668bc2fbdc05386efca1579ec87b4
msuma01 <108815660+msuma01@users.noreply.github.com>
2025-05-29 00:06:12 +0530

    Create FmPage.java

---
+++ src/main/java/com/qa/opencarts/pages/FmPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencarts.pages;
+
+public class FmPage {
+
+	String name="Jockey";
+	int music = 1890;
+  String entertainer="Ghani";
+  String point ="good";
+	public FmPage() {
+		int x=30;
+	}
+	
+}
