eee22cfa25a7c6ee6484901d6026a83443ef9c87
Suma Srivalli <morapakalasuma@gmail.com>
2025-05-29 00:13:18 +0530

    updated the code

--- src/main/java/com/qa/opencarts/pages/FmPage.java
+++ src/main/java/com/qa/opencarts/pages/FmPage.java
@@ -3,9 +3,11 @@ package com.qa.opencarts.pages;
 public class FmPage {
 
 	String name="Jockey";
+
 	int music = 1890;
   String entertainer="Ghani";
   String point ="good";
+
 	public FmPage() {
 		int x=30;
 	}
